987f5cddec18777a387136d2cc49f0e4be054379
Francisco Molina <mexicanfm@gmail.com>
2016-08-04 05:26:44 +0000

    add new files and change files

--- calculator.js
+++ calculator.js
@@ -1,1 +1,31 @@
+var a,b;
+function setValues()
+{
+a = Number(document.getElementById("a").value);
+b = Number(document.getElementById("b").value);
+}
+function sum()
+{
+setValues();
+result = a+b;
+alert("The sum is equal to " + result);
+}
+function subtract()
+{
+setValues();
+result = a-b;
+alert("The subtraction is equal to " + result);
+}
+function mult()
+{
+setValues();
+result = a*b;
+alert("The operation is equal to " + result);
+}
+function div()
+{
+setValues();
+result = a/b;
+alert("The operation is equal to " + result);
+}
 
